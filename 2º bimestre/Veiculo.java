b1e12fb585f5bbf730d3859b403559a45260ca84
lessthur <126773266+lessthur@users.noreply.github.com>
2023-04-28 11:11:13 -0300

    Update Veiculo.java

--- 2º bimestre/Veiculo.java
+++ 2º bimestre/Veiculo.java
@@ -1,17 +1,25 @@
+import java.util.Scanner;
+
 public class Veiculo {
+    public Scanner scanner = new Scanner(System.in);
     public String marca;
     public String tipo; //caminhão, moto, veículo de passeio, etc
     public String modelo;
-    public int ano;
+    public String ano;
 
-    public Veiculo(String marca, String tipo, String modelo, int ano){
-        this.marca = marca;
-        this.tipo = tipo;
-        this.modelo = modelo;
-        this.ano = ano;
+
+    public Veiculo(String marca, String tipo, String modelo, String ano){
+        System.out.println("Informe a marca do veículo: ");
+        this.marca = scanner.next();
+        System.out.println("Informe o tipo do veículo: ");
+        this.tipo = scanner.next();
+        System.out.println("Informe o modelo do veículo: ");
+        this.modelo = scanner.next();
+        System.out.println("Informe o ano do veículo: ");
+        this.ano = scanner.next();
     }
 
-    public void exibe(String marca, String tipo, String modelo, int ano){
-        System.out.println("O carro é um " + modelo + "da marca " + marca + "e ano " + ano +". É um veículo do tipo " + tipo + "."  );
+    public void exibe(){
+        System.out.println("O carro é um " + this.modelo + " da marca " + this.marca + " e ano " + this.ano +". É um veículo do tipo " + this.tipo + "."  );
     }
 }
