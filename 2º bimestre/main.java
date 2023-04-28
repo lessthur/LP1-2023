e2ba1740d7f968d34c8d3e876966c900c7fdb495
lessthur <126773266+lessthur@users.noreply.github.com>
2023-04-28 11:10:08 -0300

    Update main.java

--- 2º bimestre/main.java
+++ 2º bimestre/main.java
@@ -1,18 +1,8 @@
 import java.util.Scanner;
-public class main {
+
+public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        char resposta = 'S';
-        Veiculo modelo;
-        Veiculo ano;
-        Veiculo marca;
-        Veiculo tipo;
-
-        while (resposta == 'S'){
-            System.out.println("Qual é o modelo do carro? ");
-
-            System.out.println("Deseja informar outro veículo? Se sim, digite S, se não, digite N.");
-            resposta = scanner.next().charAt(0);
-        }
+            Veiculo veiculo = new Veiculo(" ", " ", " ", " ");
+            veiculo.exibe();
     }
 }
