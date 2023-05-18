244563eec8c321fff09ff5b49969056ac21d8bea
lessthur <126773266+lessthur@users.noreply.github.com>
2023-05-18 11:51:40 -0300

    Delete 2º bimestre/ControleDeGastos directory

--- 2º bimestre/ControleDeGastos/fixo.java
+++
@@ -1,6 +0,0 @@
-public class fixo extends gastos{
-
-    public fixo(String nome, double valor) {
-        super(nome, valor);
-    }
-}
--- 2º bimestre/ControleDeGastos/variavel.java
+++
@@ -1,8 +0,0 @@
-public class variavel extends gastos{
-    private int tempoMeses;
-
-    public variavel(String nome, double valor, int tempoMeses) {
-        super(nome, valor);
-        this.tempoMeses = tempoMeses;
-    }
-}
